f1dd568916ed12ed97beacfaa8bb54cfb4352723
AoziversaliuS <1220310838@qq.com>
2014-03-30 19:01:32 +0800

    Revert "断电"
    This reverts commit ac303f17f7abb7a1f78ce8ac3468ae4798aceb33.

--- src/gui/OTM.java
+++ src/gui/OTM.java
@@ -10,8 +10,10 @@ import bean.SoftWare;
 
 public class OTM extends AbstractTableModel{
 
+	public static final int MODE_SOFTWARE_VIEW = 1,MODE_SOFTWARE_SET = 2;
 	
 	private static ArrayList<String> rowValue;
+	private static int contentMode = 1;
 	
 	public static final int COLUMN_COUNT = 1;      //总共有多少行
 	public static final int MAX_ROW_ON_SCREEN = 11;//屏幕上能显示的最大行数
@@ -58,8 +60,41 @@ public class OTM extends AbstractTableModel{
 	}
 
 
+	public void setRowValue(ArrayList<String> rowValue) {
+		this.rowValue = rowValue;
+	}
+	public static void setModel(int MODE){
+		contentMode = MODE;
+		if(contentMode==OTM.MODE_SOFTWARE_SET){
+			rowValue = new ArrayList<String>();
+			rowValue.add("返回");
+			rowValue.add("添加");
+			rowValue.add("在此输入值");
+			rowValue.add("插入");
+			rowValue.add("在此输入值");
+			rowValue.add("删除");
+			rowValue.add("在此输入值");
+			rowValue.add("配置。。。");
+		}
+		OzFrame.ozTable.updateUI();
+		OzFrame.ozTable.repaint();
+	}
 
 
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		if(contentMode==OTM.MODE_SOFTWARE_SET){
+			if(rowIndex==2 || rowIndex==4 || rowIndex==6 ){
+				return true;
+			}
+		}
+		return false;
+	}
+
+    
+	public static int getContentModel(){
+		return contentMode;
+	}
 	
 
 }
